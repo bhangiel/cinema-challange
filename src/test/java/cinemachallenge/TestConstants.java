831e66c18524b2ac5ef42214a2642e3b2a7541ea
Bolesław Hangiel <b.hangiel@gmail.com>
2022-06-13 16:00:54 +0200

    no-ticket: room is already scheduled at the time logic added

--- src/test/java/cinemachallenge/TestConstants.java
+++ src/test/java/cinemachallenge/TestConstants.java
@@ -8,8 +8,9 @@ public abstract class TestConstants {
     public static final String SCHEDULE_ID = "AnyScheduleId";
     public static String MOVIE_NAME = "Shrek";
     public static Duration MOVIE_DURATION = Duration.of(90, ChronoUnit.MINUTES);
-    public static LocalTime SCREENING_STARTING = LocalTime.of(12, 12, 12);
+    public static LocalTime SCREENING_STARTING = LocalTime.of(12, 00, 00);
+    public static LocalTime SCREENING_STARTING_2 = LocalTime.of(14, 15, 00);
     public static String ROOM_NAME = "ROOM_1";
-    public static Duration ROOM_CLEANING_TIME = Duration.of(90, ChronoUnit.MINUTES);
+    public static Duration ROOM_CLEANING_TIME = Duration.of(30, ChronoUnit.MINUTES);
 
 }
--- src/test/java/cinemachallenge/domain/model/ScheduleTest.java
+++ src/test/java/cinemachallenge/domain/model/ScheduleTest.java
@@ -3,6 +3,7 @@ package cinemachallenge.domain.model;
 import cinemachallenge.TestConstants;
 import org.junit.jupiter.api.Test;
 import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,8 +16,8 @@ class ScheduleTest {
         var schedule = Schedule.emptySchedule(TestConstants.SCHEDULE_ID);
         var movie = new Movie(TestConstants.MOVIE_NAME, TestConstants.MOVIE_DURATION, Set.of(ScreeningType._2D));
         var room = new Room(TestConstants.ROOM_NAME, TestConstants.ROOM_CLEANING_TIME, true);
-        var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
         //when
+        var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
         var scheduleResult = schedule.schedule(screening);
         //then
         assertThat(schedule.getScreenings()).hasSize(1);
@@ -29,8 +30,8 @@ class ScheduleTest {
         var schedule = Schedule.emptySchedule(TestConstants.SCHEDULE_ID);
         var movie = new Movie(TestConstants.MOVIE_NAME, TestConstants.MOVIE_DURATION, Set.of(ScreeningType._2D));
         var room = new Room(TestConstants.ROOM_NAME, TestConstants.ROOM_CLEANING_TIME, false);
-        var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
         //when
+        var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
         var scheduleResult = schedule.schedule(screening);
         //then
         assertThat(schedule.getScreenings()).isEmpty();
@@ -38,17 +39,38 @@ class ScheduleTest {
     }
 
     @Test
-    void should_not_schedule_screening_while_room_is_already_scheduled_at_given_time() {
+    void should_not_schedule_screening_while_room_is_already_scheduled_at_given_time_with_one_screening() {
         //given
         var schedule = Schedule.emptySchedule(TestConstants.SCHEDULE_ID);
         var movie = new Movie(TestConstants.MOVIE_NAME, TestConstants.MOVIE_DURATION, Set.of(ScreeningType._2D));
-        var room = new Room(TestConstants.ROOM_NAME, TestConstants.ROOM_CLEANING_TIME, false);
+        var room = new Room(TestConstants.ROOM_NAME, TestConstants.ROOM_CLEANING_TIME, true);
         var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
+        schedule.schedule(screening);
 
         //when
         var scheduleResult = schedule.schedule(screening);
         //then
-        assertThat(schedule.getScreenings()).isEmpty();
-        assertThat(scheduleResult).isEqualTo(ScheduleResult.RoomUnavailable);
+        assertThat(scheduleResult).isEqualTo(ScheduleResult.RoomUnavailableAtGivenTime);
+        assertThat(schedule.getScreenings()).hasSize(1);
+    }
+
+    @Test
+    void should_not_schedule_screening_while_room_is_already_scheduled_at_given_time_with_two_screenings_in_one_room() {
+        //given
+        var schedule = Schedule.emptySchedule(TestConstants.SCHEDULE_ID);
+        var movie = new Movie(TestConstants.MOVIE_NAME, TestConstants.MOVIE_DURATION, Set.of(ScreeningType._2D));
+        var room = new Room(TestConstants.ROOM_NAME, TestConstants.ROOM_CLEANING_TIME, true);
+        var screening = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING, room);
+        var screening2 = new Screening(movie, DayOfWeek.MONDAY, TestConstants.SCREENING_STARTING_2, room);
+        schedule.schedule(screening);
+        schedule.schedule(screening2);
+
+        //when
+        var scheduleTime = LocalTime.of(14, 00);
+        var screening3 = new Screening(movie, DayOfWeek.MONDAY, scheduleTime, room);
+        var scheduleResult = schedule.schedule(screening3);
+        //then
+        assertThat(scheduleResult).isEqualTo(ScheduleResult.RoomUnavailableAtGivenTime);
+        assertThat(schedule.getScreenings()).hasSize(2);
     }
 }
